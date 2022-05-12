9d4649c0af308e38411b3aa060c69c10b3cb21b1
AntaripSaha <antarip15@gmail.com>
2022-05-12 18:09:54 +0600

    merged

--- resources/views/includes/company_dashboard_stats.blade.php
+++ resources/views/includes/company_dashboard_stats.blade.php
@@ -2,12 +2,14 @@
     <li class="col-md-4 col-6">
         <div class="inbox"> <i class="fa fa-clock-o" aria-hidden="true"></i>
             <h6><a href="{{route('posted.jobs')}}">{{Auth::guard('company')->user()->countOpenJobs()}}</a></h6>
-            <strong>{{__('Open Jobs')}}</strong> </div>
+            <strong>{{__('Open Jobs')}}</strong> 
+        </div>
     </li>
     <li class="col-md-4 col-6">
         <div class="inbox"> <i class="fa fa-user-o" aria-hidden="true"></i>
             <h6><a href="{{route('company.followers')}}">{{Auth::guard('company')->user()->countFollowers()}}</a></h6>
-            <strong>{{__('Followers')}}</strong> </div>
+            <strong>{{__('Followers')}}</strong> 
+        </div>
     </li>
     {{-- <li class="col-md-4 col-6">
         <div class="inbox"> <i class="fa fa-envelope-o" aria-hidden="true"></i>
